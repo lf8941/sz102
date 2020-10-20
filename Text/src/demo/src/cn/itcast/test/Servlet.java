1f1b44d3c266db71c39714c055f593013d533072
111 <470000791@qq.com>
2020-10-20 11:26:44 +0800

    添加文件

---
+++ Text/src/demo/src/cn/itcast/test/Servlet.java
@@ -0,0 +1,29 @@
+package demo.src.cn.itcast.test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * @Author LiuFeng
+ * @Date 2020/9/26 9:26
+ */
+@WebServlet("/addAccount")
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
